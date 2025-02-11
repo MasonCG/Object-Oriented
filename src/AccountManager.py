d0edbbfc7d2de599f3aa90fc119fe6143f98e495
Mason Goddard <Business.goddardm@gmail.com>
2025-02-11 14:22:52 -0500

    creating unit tests 2 branch

---
+++ src/AccountManager.py
@@ -0,0 +1,16 @@
+class AccountManager:
+    #saving info for accounts
+
+    #method for creating accounts
+
+    #method for updating account info
+
+    #method for info of specific account
+
+    #method for verifying user
+
+    
+
+
+
+    pass
---
+++ tests/testAccountManager.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class TestAccountManager(TestCase):
+    def tset_verifyUser(self):
+        pass
